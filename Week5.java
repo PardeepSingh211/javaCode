7eac468c09122a93b199ff2752c4f82c8beb2384
PardeepSingh211 <147282550+PardeepSingh211@users.noreply.github.com>
2023-10-07 18:01:09 -0400

    Add files via upload
    week5 javacode

---
+++ Week5.java
@@ -0,0 +1,18 @@
+package fall2023;
+
+public class Week5 {
+
+	public static void  sum(int a, int b) {
+		int s=0;
+		for(int i=a;i<=b;i++)
+		{
+			s+=i;
+			
+		}
+		System.out.println("the value of s is"+s);
+		
+		
+
+	}
+
+}
